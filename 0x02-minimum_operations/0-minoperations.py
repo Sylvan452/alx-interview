d86ee40e0ebc9909168bf26b7ee35ea000db0f98
sylvan452 <sylvan452@gmail.com>
2023-08-12 15:36:30 +0000

    white space removed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-A module defines a function that calculates the fewestnumber of 
+A module defines a function that calculates the fewestnumber of
 operations needed to result in exactly n H characters in the a given file
 """
 
 
 def minOperations(n):
     """
-    This returns the fewest number of operations needed to result in 
+    This returns the fewest number of operations needed to result in
     exactly n H characters in a given file.
     """
     if type(n) is not int or n <= 1:
@@ -16,12 +16,12 @@ def minOperations(n):
     factor = 2
     while (n % factor) is 0 and (n // factor) is not 1:
         summation.append(factor)
-        n = n //factor 
+        n = n // factor
     factor = 3
     while n > factor:
         while (n % factor) is 0 and (n // factor) is not 1:
             summation.append(factor)
-            n = n //factor 
+            n = n // factor
         factor += 2
     summation.append(n)
     return sum(summation)
