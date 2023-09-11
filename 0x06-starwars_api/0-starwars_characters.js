4747c9f40692c5197362155f9c5f31120e51733f
sylvan452 <sylvan452@gmail.com>
2023-09-11 18:58:10 +0000

    corrected

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
+const axios = require('axios');
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
+  const filmId = process.argv[2];
+  const API_URL = `https://swapi.dev/api/films/${filmId}/`;
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+  axios.get(API_URL)
+    .then(response => {
+      const charactersURL = response.data.characters;
+      const characterPromises = charactersURL.map(url => axios.get(url));
+
+      Promise.all(characterPromises)
+        .then(responses => {
+          const characterNames = responses.map(response => response.data.name);
+          console.log(characterNames.join('\n'));
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
+
