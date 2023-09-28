da99821ab98d3f6cad5bde823b5e5ca2bae77bb5
sylvan452 <sylvan452@gmail.com>
2023-09-28 10:48:17 +0000

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Define island_perimeter function that finds the perimeter
+of an island in a body of water
+"""
+
+def island_perimeter(grid):
+    if not grid:
+        return 0
+    
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    
+    return perimeter
